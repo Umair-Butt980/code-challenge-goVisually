12aeb2417268c38cbf135b42b1a3bd9d7e23865b
UmairButt <umairamjadbutt1@gmail.com>
2022-09-14 13:13:39 +0500

    comment added

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ app.use("/", express.static(path.join(__dirname, "./client/build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
-
+//Port
 app.listen(process.env.PORT || 5000);
